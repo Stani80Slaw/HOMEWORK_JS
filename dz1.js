0bbc23a57e74ede7db9851e99d21d061f8ce416a
Stanislaw P <Stanislav.Perederenko@tptlive.ee>
2023-05-07 14:36:44 +0300

    first commit

---
+++ dz1.js
@@ -0,0 +1,83 @@
+// Напишите программу которая сложит все числа в заданном массиве
+// выведет результат в консоль
+const nums1 = [5, 6, 92, 47, 12, -18, 33, 8];
+let sum = 0;
+
+nums1.forEach(num => {
+    sum += num
+    })
+console.log(sum);
+
+
+
+// Напишите программу которая добавит в массив namesObject объекты
+// с двумя парами { name: "имя с большой буквы", nameLength: "длина имени"}
+const names = ['jack', 'sarah', 'mary', 'joey', 'chris', 'samantha'];
+const namesObject  =[];
+
+names.forEach( naam => {
+    namesObject.push({
+    name: naam.charAt(0).toUpperCase()+naam.slice(1),
+    nameLength:   naam.length,
+    }
+    )
+})
+console.log(namesObject);
+
+
+
+// Напишите программу которая в массив numsRes добавит объекты
+// с тремя парами { number: "само число", square: "число в квадрпате", cube: "число в кубе"}
+const nums2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const numsRes = [];
+
+nums2.forEach( num =>{
+    numsRes.push({
+        numbers:num,
+        square:num**2,
+        cube:num**3,
+    })
+})
+console.log(numsRes);
+
+
+// Используйте forEach чтобы в массив 'reversedNumberRow'
+// добавить те же числа, но в обратном порядке.
+const numberRow = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const reversedNumberRow = [];
+
+numberRow.forEach(revers =>{
+    reversedNumberRow.unshift(revers);
+})
+console.log(reversedNumberRow);
+
+
+// Для каждой машины в массиве используйте "forEach"
+// что-бы вывести в консоль строку типа:
+// This is green Jaguar. It costs 7000€.
+
+const cars = [
+    {
+        make: 'BMW',
+        color: 'grey',
+        price: 10000
+    },
+    {
+        make: 'Mercedes-Benz',
+        color: 'black',
+        price: 15000
+    },
+    {
+        make: 'Honda',
+        color: 'blue',
+        price: 8000
+    },
+    {
+        make: 'Ferrari',
+        color: 'red',
+        price: 50000
+    } 
+];
+cars.forEach( car=>{
+    console.log(`This is ${(car.color)} ${(car.make)}. It costs ${(car.price)}€.`);
+})
---
+++ dz2.js
@@ -0,0 +1,160 @@
+// напишите программу которая выводит в консоль сумму всех
+// четных чисел в массиве
+
+const numsArray = [1, 12, 34, 71, 14, 12, 33, 70, 82, 81, 9, 19, 90];
+let sum = 0;
+
+numsArray.forEach(num => {
+    if(num%2==0){
+        sum += num
+    }
+})
+console.log(sum);
+
+
+
+
+// Напишите программу которая проанализирует данный массив с объектами
+// и добавит в новый массив только имена тех людей кому 18 и старше
+
+const people = [
+    {
+        name: 'Jack',
+        age: 15
+    },
+    {
+        name: 'Sarah',
+        age: 21
+    },
+    {
+        name: 'Bob',
+        age: 54
+    },
+    {
+        name: 'Mary',
+        age: 12
+    },
+    {
+        name: 'Simon',
+        age: 18
+    },
+    {
+        name: 'Jonhatan',
+        age: 17
+    }
+]
+const adults = [];
+
+people.forEach(pepl => {
+    if(pepl.age >= 18){
+        adults.push(pepl.name)
+    }
+})
+console.log(adults);   
+
+
+
+// напишите программу которая проанализирует данный массив и выведет в консоль самую длинную строку
+const strings = ['Star', 'Planet', 'Comet', 'Interstellar', 'Space'];
+let wordLength = '';
+
+strings.forEach( wlength => {
+    if(wlength.length > wordLength.length){
+        wordLength =  wlength;
+    }
+})
+console.log(wordLength);
+
+
+// напишите программу которая возьмёт из данного массива наименования книг которые вышли в этом году
+// и добавит их в новый массив
+const books = [
+    {
+        author: 'Jeremy Brook',
+        title: 'My childhood',
+        release: 2023
+    },
+    {
+        author: 'Samantha Jhones',
+        title: 'Living with ten cats',
+        release: 2020
+    },
+    {
+        author: 'Bob Summers',
+        title: 'Exploring far space',
+        release: 2021
+    },
+    {
+        author: 'Bill Brown',
+        title: 'Insects in our garden',
+        release: 2023
+    },
+    {
+        author: 'Jessica Love',
+        title: 'Programming for begginers',
+        release: 2023
+    }
+];
+const newBooks = [];
+
+books.forEach(book=>{
+    if(book.release==2023){
+        newBooks.push({
+            author: book.author,
+            title: book.title,
+        })
+    }
+})
+console.log(newBooks);
+
+
+// Напишите программу которая проанализирует данный массив и найдёт в нем наибольшее число кратное трём
+
+const numbers = [3, 15, 23, 56, 66, 71, 12, 90];
+let bigNum = 0;
+
+numbers.forEach(num=>{
+    if(num%3==0){
+        bigNum = num;
+    }
+})
+console.log(bigNum);
+
+
+// напишите программу которая найдёт самого старшего человека в данном массиве с объектами
+// и сохранит его в переменную oldestPerson
+const people2 = [
+    {
+        name: 'Jack',
+        age: 15
+    },
+    {
+        name: 'Sarah',
+        age: 21
+    },
+    {
+        name: 'Bob',
+        age: 54
+    },
+    {
+        name: 'Mary',
+        age: 12
+    },
+    {
+        name: 'Simon',
+        age: 18
+    },
+    {
+        name: 'Jonhatan',
+        age: 17
+    }
+];
+let oldestPerson ='';
+let ageBig= 0;
+people2.forEach(people=>{
+    if(people.age > ageBig ){
+        ageBig = people.age;
+        oldestPerson = people.name;
+    }
+})
+console.log(oldestPerson);
