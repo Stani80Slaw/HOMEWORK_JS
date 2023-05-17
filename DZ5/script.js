c3f67453e8f9983cf12bb1fb0ac39191e2df0052
Stanislaw P <Stanislav.Perederenko@tptlive.ee>
2023-05-17 23:59:38 +0300

    DZ5

---
+++ DZ5/script.js
@@ -0,0 +1,6 @@
+function color(){
+    const figure = document.querySelector('.figure');
+    const textColor= document.querySelector('#textColor');
+    figure.style.backgroundColor =textColor.value;
+}
+
